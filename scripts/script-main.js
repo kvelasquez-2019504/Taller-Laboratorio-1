ee73df4854f478da74ddcd03c3cf8d26510f6ec9
Kenneth Velasquez <kvelasquez-2019504@kinal.edu.gt>
2024-01-25 11:40:50 -0600

    Se agregó un reloj funcional con js en el index

--- scripts/script-main.js
+++ scripts/script-main.js
@@ -1,3 +1,10 @@
 function obtenerHora(){
-    
+    let reloj = document.getElementById("reloj");
+    var fechaHora= new Date();
+    let hora = fechaHora.getHours();
+    let minuto = fechaHora.getMinutes();
+    let segundos = fechaHora.getSeconds();
+    let horaActual = hora +":"+minuto+":"+segundos;
+    reloj.value = horaActual;
+    setTimeout("obtenerHora()",1000);
 }
