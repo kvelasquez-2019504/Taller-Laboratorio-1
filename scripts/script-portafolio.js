ffab8fae12e0e48c750c7a3bc09feecb0d7659f6
Kenneth Velasquez <kvelasquez-2019504@kinal.edu.gt>
2024-01-25 21:52:52 -0600

    Se terminó de agregar las barras

--- scripts/script-portafolio.js
+++ scripts/script-portafolio.js
@@ -1,63 +1,101 @@
-var progresos = 0;
-var progresoJava =0;
-function mostrarProgreso() {
-	/*let bar = document.getElementById("bar");
-    console.log(progreso);
-	if(progreso<55){
-        progreso+=5;
-        bar.style.width = progreso+"%";
-        setTimeout("mostrarProgreso()",1000);
-        bar.innerHTML = progreso+"%";
-    }else{
-        setTimeout("progreso=0;mostrarProgreso()",5000);
-        bar.innerHTML = progreso;
-    }*/
+var progresos=0;
+var progresoJava=0;
+var progresoJavaEE=0;
+var progresoSQL=0;
+var progresoJS=0;
+var progresoHTML=0;
+var progresoCSS=0;
+function cargarBarras() {
     if(progresos == 6){
+        progresos=0;
         cargarBarras();
     }else{
-        barJava();
-       /* barJavaEE();
-        barSQL();
-        barJS();
-        barHTML();
-        barCSS();*/
+        if(progresos==0){
+            barJava();
+            barJavaEE();
+            barSQL();
+            barJS();
+            barHTML();
+            barCSS();
+        }
     }
 }
 
 function barJava(){
     let barJava = document.getElementById("barJava");
-    if(progresoJava<70){
+    if(progresoJava<65){
         progresoJava+=5;
         barJava.style.width = progresoJava+'%';
         barJava.innerHTML = progresoJava+"%";
         setTimeout("barJava()",1000);
     }else{
         progresoJava=0;
-        progresos+1;
+        progresos+=1;
+        cargarBarras();
     }
 }
 function barJavaEE(){
-    if(progreso <61){
-
+    let barJavaEE = document.getElementById("barJavaEE");
+    if(progresoJavaEE <60){
+        progresoJavaEE+=5;
+        barJavaEE.style.width = progresoJavaEE+'%';
+        barJavaEE.innerHTML = progresoJavaEE+"%";
+        setTimeout("barJavaEE()",1000);
+    }else{
+        progresoJavaEE=0;
+        progresos+=1;
+        cargarBarras();
     }
 }
 function barSQL(){
-    if(progreso <56){
-
+    let barSQL= document.getElementById("barSQL");
+    if(progresoSQL <55){
+        progresoSQL+=5;
+        barSQL.style.width = progresoSQL+"%";
+        barSQL.innerHTML = progresoSQL+"%";
+        setTimeout("barSQL()",1000);
+    }else{
+        progresoSQL=0;
+        progresos+=1;
+        cargarBarras();
     }
 }
 function barJS(){
-    if(progreso<41){
-
+    let barJS = document.getElementById("barJS");
+    if(progresoJS<40){
+        progresoJS+=5;
+        barJS.style.width = progresoJS+"%";
+        barJS.innerHTML = progresoJS+"%";
+        setTimeout("barJS()",1000);
+    }else{
+        progresoJS=0;
+        progresos+=1;
+        cargarBarras();
     }
 }
 function barHTML(){
-    if(progreso >46){
-
+    let barHTML = document.getElementById("barHTML");
+    if(progresoHTML<45){
+        progresoHTML+=5;
+        barHTML.style.width = progresoHTML+"%";
+        barHTML.innerHTML = progresoHTML+"%";
+        setTimeout("barHTML()",1000);
+    }else{
+        progresoHTML=0;
+        progresos+=1;
+        cargarBarras();
     }
 }
 function barCSS(){
-    if(progreso >46){
-
+    let barCSS = document.getElementById("barCSS");
+    if(progresoCSS <45){
+        progresoCSS+=5;
+        barCSS.style.width = progresoCSS+"%";
+        barCSS.innerHTML = progresoCSS+"%";
+        setTimeout("barCSS()",1000);
+    }else{
+        progresoCSS=0;
+        progresos+=1;
+        cargarBarras();
     }
 }
